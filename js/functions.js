f178392ea4bd388d06869f7d6498e3d14b4e15b2
Vedrana <vedranaha@gmail.com>
2022-03-16 23:43:58 +0100

    cartList

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,23 @@
 //DESCRIPTION: Devuelve la suma de los subtotales por categoría del carrito.
 //AUTOR: Sandra Sarmiento
 function calculateTotal() {
-    let sum = 0
-    for (const key in subtotal) {
-      sum += subtotal[key].value
-    }
-    return sum
+  let sum = 0;
+  for (const key in subtotal) {
+    sum += subtotal[key].value;
   }
-  //PROTOTYPE:  Void getAllProducts ()
+  return sum;
+}
+//PROTOTYPE:  Void getAllProducts ()
 //DESCRIPTION: Muestra por consola todos los productos.
 //AUTOR: Sandra Sarmiento
 function getAllProducts() {
-    products.forEach(product => {
-      console.table(product)
-    });
+  products.forEach((product) => {
+    console.table(product);
+  });
+}
+// GenerateCart(): receives the cartList array
+function generateCart() {
+  for (let i = 0; i < cart.length; i++) {
+    console.log("Product: " + cart[i].name + "Price:" + cart[i].price);
   }
+}
