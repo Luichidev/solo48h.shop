cc29d9df7cbcd2585ed6805225a3551a4e32272f
dfanttini <fanttini@hotmail.com>
2022-03-24 13:41:56 +0100

    feat: test-functions-Dorian

--- js/testfunctions-dorian.js
+++ js/testfunctions-dorian.js
@@ -1,13 +1,9 @@
-//PROTOTYPE:  Void orderByProduct()
-//DESCRIPTION: Ordena en forma descendente los productos dentro de cart.
-//AUTOR: Dorian Fanttini
-// buy(1)
-// buy(3)
-// buy(2)
-// buy(4)
-// buy(8)
+//Funcion ordenar por producto
 
-generateCart()
+orderByProduct()
+console.table(Productos)
+
+//Funcion ordenar por categoria
 
 orderByCategory()
 console.table(Productos)
