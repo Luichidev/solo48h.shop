16a834ebdf170e4ff908fb8d7b814adbc039b90a
@LuisArana <disconluis@gmail.com>
2022-03-31 16:23:13 +0200

    feat: Fix url images of products

--- js/controlador.js
+++ js/controlador.js
@@ -1,31 +1,36 @@
 //Aqui va el código que enlazará las funciones con el DOM
-window.addEventListener("DOMContentLoaded", () => {
-  printProductsInCategories();
-});
-
-function printProductsInCategories() {
-  const cards = document.getElementById("cards");
-  const templateCard = document.getElementById("template-card").content;
-  const fragment = document.createDocumentFragment();
-
-  Productos.forEach((producto) => {
-    templateCard.querySelector(".card-img").setAttribute("src", producto.url);
-    templateCard.querySelector(".card-body > p").textContent = producto.titulo;
-    templateCard.querySelector(".card-product__title > a").textContent =
-      producto.categoria;
-    templateCard.querySelector(".card-product__price").textContent =
-      producto.precio;
-    const clone = templateCard.cloneNode(true);
-    fragment.appendChild(clone);
-  });
-  cards.appendChild(fragment);
-}
+/**
+ * @Variables Glovales
+ */
 
 let count = 0
 const cartIcon = document.querySelector(
   '.nav-item > button > .ti-shopping-cart'
 )
 const cartIconNumber = document.querySelector('.nav-shop__circle')
+
+window.addEventListener('DOMContentLoaded', () => {
+  printProductsInCategories()
+})
+
+// Pintar los productos en la pagina de categorias
+function printProductsInCategories() {
+  const cards = document.getElementById('cards')
+  const templateCard = document.getElementById('template-card').content
+  const fragment = document.createDocumentFragment()
+
+  Productos.forEach((producto) => {
+    templateCard.querySelector('.card-img').setAttribute('src', producto.url)
+    templateCard.querySelector('.card-body > p').textContent = producto.titulo
+    templateCard.querySelector('.card-product__title > a').textContent =
+      producto.categoria
+    templateCard.querySelector('.card-product__price').textContent =
+      producto.precio
+    const clone = templateCard.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  cards.appendChild(fragment)
+}
 
 //Click en el icono de carrito
 cartIconNumber.innerHTML = count
