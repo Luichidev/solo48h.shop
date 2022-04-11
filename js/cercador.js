bdb0d38957c90adb5cf89ad647163ae6024dd40d
almaocuoc <almaocnissan@gmail.com>
2022-04-11 13:30:23 +0200

    search add function with category print

--- js/cercador.js
+++ js/cercador.js
@@ -5,5 +5,6 @@ prodSearch.addEventListener('click', e => {
     let prodWordSearched = document.getElementById('busca').value
     const resposta= Productos.filter(element => element.titulo.trim().toLowerCase().includes(prodWordSearched.trim().toLowerCase()))
     console.log(resposta);
+    printProductsInCategories(resposta)
 });
 
